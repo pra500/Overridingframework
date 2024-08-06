54006bd664bfd863543c6ec40f88b026f2c4b1de
pragti gupta <pragtig6@gmail.com>
2024-08-06 13:09:08 +0530

    gaa

---
+++ src/test/java/pac1/Base.java
@@ -0,0 +1,57 @@
+package pac1;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class Base {
+	
+	
+
+ WebDriver driver=null;	
+String browsername;
+	
+	 
+	
+	public void initialization()
+	{
+		
+	if(driver==null)
+	{
+		if(browsername.equalsIgnoreCase("chrome"))
+		{
+			 driver=new ChromeDriver();	
+		}
+		else if(browsername.equalsIgnoreCase("firefox"))
+		{
+			 driver=new FirefoxDriver();
+		}
+		else if(browsername.equalsIgnoreCase("edge"))
+		{
+			driver=new EdgeDriver();
+		}
+			
+	}		
+	
+	
+	    driver.manage().window().maximize();
+		driver.manage().deleteAllCookies();
+		driver.manage().timeouts().pageLoadTimeout(20, TimeUnit.SECONDS);
+		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+		driver.get("https://naveenautomationlabs.com/opencart/");
+
+}	
+	
+	
+
+public void verifyTitle()
+{
+	String title=driver.getTitle();
+	System.out.println(title);
+	
+}
+
+}
---
+++ src/test/java/pac1/productpageTest.java
@@ -0,0 +1,23 @@
+package pac1;
+
+import org.testng.annotations.Test;
+
+public class productpageTest extends Base {
+	
+	@Override
+	public void initialization()
+	{
+		
+		System.out.println("this is my browsererrr");
+	}
+	
+	@Override
+	public void verifyTitle()
+	{
+		
+
+		System.out.println("this is my titleeeeeeeeeee");
+	}
+	
+ 
+}
